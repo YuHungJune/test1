65e8fc110509ea38c468e4d383822fb8f1d9395d
YuHungJune <batort08@gmail.com>
2022-01-08 10:34:04 +0900

    test1

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class test1 {
     public static void main(String[] args){
         System.out.println("Hello");
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(n);
+
     }
 }
